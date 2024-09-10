f6e5fcfe113823cd4455e28e76a3918069b5829f
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-09-10 23:38:07 +0700

    tugas 2

--- main/models.py
+++ main/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-class MoodEntry(models.Model):
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField()
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_main(request):
+def show_product(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'name' : 'Desmond Regamaster Evo II Forged Alloy Wheel',
+        'price' : '25.000.000',
+        'description' : '19x9 ET45 5x100 Almighty Grey',
+        'image' : 'https://www.torque-gt.co.uk/media/catalog/product/r/e/regamaster_evo_2_almighty_grey_1_1.png?width=800&height=800&store=default&image-type=image'
     }
+    return render(request, 'main.html', context)
 
-    return render(request, "main.html", context)
