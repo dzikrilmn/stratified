cde7d8331cab0cf363a96e06734d5ccbfc756a5b
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-09-11 11:22:21 +0700

    tugas 2.4

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField()
+    # image = models.ImageField()
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_product(request):
         'name' : 'Desmond Regamaster Evo II Forged Alloy Wheel',
         'price' : '25.000.000',
         'description' : '19x9 ET45 5x100 Almighty Grey',
-        'image' : 'https://www.torque-gt.co.uk/media/catalog/product/r/e/regamaster_evo_2_almighty_grey_1_1.png?width=800&height=800&store=default&image-type=image'
+        # 'image' : 'https://www.torque-gt.co.uk/media/catalog/product/r/e/regamaster_evo_2_almighty_grey_1_1.png?width=800&height=800&store=default&image-type=image'
     }
     return render(request, 'main.html', context)
 
