22b5f9a7458bd5894fd098739d8549b16f7bae7b
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-09-17 14:01:35 +0700

    tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from main.views import show_product
+# from main.views import show_product
+from main.views import show_main
+from main.views import show_main, create_product_entry
+from main.views import show_main, create_product_entry, show_xml
+from main.views import show_main, create_product_entry, show_xml, show_json
+
+
+
+
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_product, name='show_product'),
+    path('', show_main, name='show_main'),
+    path('create-product-entry', create_product_entry, name='create_product_entry'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,13 +1,58 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from main.models import Product
+from main.forms import ProductEntryForm
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
-def show_product(request):
+
+def show_main(request): 
+    product_entry = Product.objects.all()
     context = {
-        'name' : 'Desmond Regamaster Evo II Forged Alloy Wheel',
-        'price' : '25.000.000',
-        'description' : '19x9 ET45 5x100 Almighty Grey',
-        'uname' : 'Muhammad Dzikri Ilmansyah',
-        'uclass' : 'PBP C',
-        # 'image' : 'https://www.torque-gt.co.uk/media/catalog/product/r/e/regamaster_evo_2_almighty_grey_1_1.png?width=800&height=800&store=default&image-type=image'
+        'uname': 'Muhammad Dzikri Ilmansyah',
+        'uclass': 'PBP C',
+        # 'name' : 'Desmond Regamaster Evo II Forged Alloy Wheel',
+        # 'price' : '25.000.000',
+        # 'description' : '19x9 ET45 5x100 Almighty Grey',
+        'product_entry' : product_entry
     }
-    return render(request, 'main.html', context)
+
+    return render(request, "main.html", context)
+
+
+# def product_entry(request):
+#     product_entry = Product.objects.all()
+#     context = {
+#         'name' : 'Desmond Regamaster Evo II Forged Alloy Wheel',
+#         'price' : '25.000.000',
+#         'description' : '19x9 ET45 5x100 Almighty Grey',
+#     }
+#     return render(request, 'product_entry.html', context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
