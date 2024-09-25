663dee8a4119c53857bfceb2e0f5e20faddf3a7b
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-09-25 09:34:31 +0700

    tugas 4.2

--- main/models.py
+++ main/models.py
@@ -11,6 +11,3 @@ class Product(models.Model):
     def __str__(self):
         return self.name
         
-class Person(models.Model):
-    nama = models.CharField(max_length=255)
-    age = models.DecimalField(max_digits=3)    
