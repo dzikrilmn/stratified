91e7503374aaf3672e005ba7aa6043021b4008e3
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-10-01 21:43:51 +0700

    tugas 5.4

--- main/forms.py
+++ main/forms.py
@@ -9,6 +9,6 @@ class ProductEntryForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'border p-2 w-full rounded text-black'}),
             'price': forms.NumberInput(attrs={'class': 'border p-2 w-full rounded text-black'}),
-            'description': forms.Textarea(attrs={'class': 'border p-2 w-full rounded text-black'}),
+            'description': forms.Textarea(attrs={'class': 'border p-2 w-full rounded text-white'}),
             'image': forms.ClearableFileInput(attrs={'class': 'border p-2 w-full rounded text-white'}),  # For image upload
         }
